69810a754343a719b15a86c8121524df1b3f343f
mugid <bekslambek22@gmail.com>
2025-07-27 20:23:57 +0500

    feat: add cache

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,12 @@
-import { betterAuth } from 'better-auth';
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { db } from '@/lib/db';
-import * as schema from '@/lib/db/schema';
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/lib/db";
+import * as schema from "@/lib/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: 'pg',
-    schema
+    provider: "pg",
+    schema,
   }),
   socialProviders: {
     google: {
@@ -16,8 +16,13 @@ export const auth = betterAuth({
   },
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24,
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
   },
 });
 
 export type Session = typeof auth.$Infer.Session;
-export type User = typeof auth.$Infer.Session['user'];
+export type User = (typeof auth.$Infer.Session)["user"];
